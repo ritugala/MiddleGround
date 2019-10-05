95bdfd103c31a4b19b4da1f55670ec282b9c248f
rachanac16 <47475101+rachanac16@users.noreply.github.com>
2019-10-06 02:15:29 +0530

    Add files via upload

---
+++ add_remove.js
@@ -0,0 +1,41 @@
+$(document).on('click', '.btn-add', function(e)
+        {
+
+            
+            e.preventDefault();
+    
+            var controlForm = $('.controls form:first'),
+                currentEntry = $(this).parents('.entry:first'),
+                newEntry = $(currentEntry.clone()).appendTo(controlForm);
+
+            newEntry.find('input').val('');
+
+            controlForm.find('.entry:not(:last) .btn-add')
+                .removeClass('btn-add').addClass('close')
+                .removeClass('btn-succ').addClass('close')
+                .html("x");
+                
+        }).on('click', '.close', function(e)
+        {
+            $(this).parents('.entry:first').remove();
+            
+            e.preventDefault();
+            return false;
+        }).on('click', '.form-control', function(){
+            
+            var input_added = document.querySelectorAll('.form-control');
+
+            // Set their ids
+            for (var i = 0; i < input_added.length; i++)
+                {
+                    input_added[i].id = 'abc-' + i;
+                    input_added[i].name='xyz-' + i;
+                }
+               
+            var currentInp = $(this).attr("id");
+            
+           
+           
+           console.log(document.getElementById(currentInp).name);
+            var placeBox = new google.maps.places.Autocomplete(document.getElementById(currentInp));
+        });
