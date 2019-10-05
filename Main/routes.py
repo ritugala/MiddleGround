54dbb3b020e6eed61b28cfc5ab9ed471f91e4fa1
rachanac16 <47475101+rachanac16@users.noreply.github.com>
2019-10-06 02:56:35 +0530

    Update routes.py

--- Main/routes.py
+++ Main/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect, request
-
+from Main.forms import LocationForm
 
 from Main import app
 
@@ -8,7 +8,20 @@ from Main import app
 def home():
     return render_template("home.html")
 
-@app.route("/MeetInTheMiddle")
+@app.route("/dynamic", methods=(['GET', 'POST']))
 def Meet():
-    return render_template("")
-
+    
+    form = LocationForm()
+    
+    if form.final_submit.data==True or form.new_user.data==True:
+        n=request.values.get("hide_f")
+       
+        for s in range(int(n)):
+          print(request.values.get("xyz-"+str(s)))
+           # print("hello")
+        
+        return render_template("dynamic.html", form=form)
+    else:
+        print(form.new_user.data, form.final_submit.data, form.address1.data)
+        print("elseHello")
+        return render_template("dynamic.html", form=form)
