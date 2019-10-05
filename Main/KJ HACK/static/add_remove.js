d664171553c4c04c7dab783492389cc00a806da9
rachanac16 <47475101+rachanac16@users.noreply.github.com>
2019-10-05 20:13:08 +0530

    Update add_remove.js

--- Main/KJ HACK/static/add_remove.js
+++ Main/KJ HACK/static/add_remove.js
@@ -1,3 +1,4 @@
+
 $(document).on('click', '.btn-add', function(e)
         {
             e.preventDefault();
@@ -5,8 +6,11 @@ $(document).on('click', '.btn-add', function(e)
             var controlForm = $('.controls form:first'),
                 currentEntry = $(this).parents('.entry:first'),
                 newEntry = $(currentEntry.clone()).appendTo(controlForm);
-    
+
             newEntry.find('input').val('');
+            
+            
+            
             controlForm.find('.entry:not(:last) .btn-add')
                 .removeClass('btn-add').addClass('close')
                 .removeClass('btn-succ').addClass('close')
@@ -14,7 +18,18 @@ $(document).on('click', '.btn-add', function(e)
         }).on('click', '.close', function(e)
         {
             $(this).parents('.entry:first').remove();
-    
+            
             e.preventDefault();
             return false;
+        }).on('click', '.form-control', function(){
+            
+            var input_added = document.querySelectorAll('.form-control');
+
+            // Set their ids
+            for (var i = 0; i < input_added.length; i++)
+                input_added[i].id = 'abc-' + i;
+
+            var currentInp = $(this).attr("id");
+            
+            var placeBox = new google.maps.places.Autocomplete(document.getElementById(currentInp));
         });
